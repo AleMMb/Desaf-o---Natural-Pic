8a403a0dc589ec8df37ed8877558f8287b7e13d0
AleMMb <alejandramellab@gmail.com>
2022-12-29 12:44:50 -0300

    Se agrega mensaje no hay fotos favoritas

--- src/views/Favoritos.js
+++ src/views/Favoritos.js
@@ -7,13 +7,18 @@ export default function Favoritos() {
 
   const { imagenes } = useContext(MyContext)
 
+  let cantImagenesFavoritas = (imagenes.filter((e) => e.liked)).length
+  console.log (cantImagenesFavoritas)
+
   return (
     <div className="favoritos">
       <h1>Fotos favoritas</h1>
       <div className="galeria-favoritos">
-        {imagenes.filter((e) => e.liked).map((e) => (
-          <img key={e.id} src={e.size}></img>
-        ))}
+        {cantImagenesFavoritas != 0 ? imagenes.filter((e) => e.liked).map((e) => (
+          <div key={e.id}>
+          <img src={e.size}></img>
+          <p> Photography taken by: {e.creator} </p> </div>
+        )) : `No has seleccionado fotografías favoritas`}
       </div>
     </div>
   );
